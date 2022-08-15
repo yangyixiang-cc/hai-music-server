f57e96c32a5f912ee50b8064c21a5bf73bd9853b
yanghi <191894349@qq.com>
2022-08-15 22:42:04 +0800

    init

---
+++ src/main/java/com/yanghi/haimusic/mapper/SongSheetAndSongMapper.java
@@ -0,0 +1,21 @@
+package com.yanghi.haimusic.mapper;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yanghi.haimusic.bean.Song;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 歌单关联歌曲映射类
+ */
+
+@Mapper
+public interface SongSheetAndSongMapper{
+
+    //根据歌单id返回所有歌单中包含的歌曲分页数据
+    Page<Song> selectSongPageBySongSheetId(Page<Song> page, @Param("id") Integer id);
+
+}
